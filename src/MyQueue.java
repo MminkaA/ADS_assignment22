cd85bf70ea3f9a734247f5b7ed757170fb9edff4
Nurislam <mejramovn@gmail.com>
2024-04-21 23:08:16 +0500

    Add file

---
+++ src/MyQueue.java
@@ -0,0 +1,17 @@
+import java.util.NoSuchElementException;
+
+public class MyQueue<T extends Comparable<T>> {
+    private final MyLinkedList<T> queue;
+
+    public MyQueue() {
+        queue = new MyLinkedList<>();
+    }
+
+    public boolean IsEmpty() {
+        return queue.size == 0;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+}
